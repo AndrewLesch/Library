7000f73e5d855aed3af938e71b88a369e6c100d7
AndrewLesch <Andreykabreamz@gmail.com>
2023-10-15 20:32:22 +0300

    Create Book page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import '@/app/globals.css'
 import QuoteSlider from '@/components/Slider/Slider';
+import Link from 'next/link';
 
 export default function Home() {
   const quotes = [
@@ -21,7 +22,7 @@ export default function Home() {
         <h3 className="text-xl mt-5 pb-32">Lets add a few books and create some reviews! Enjoy!</h3>
         <QuoteSlider quotes={quotes} />
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
-          Add book
+          <Link href='/book'>Add book</Link>
         </button>
       </div>
       <div className="container w-2/5 text-center">IMG</div>
